6e2fe702df328679e9d3d108035b734d0414bec5
George Anthony Santana Rosario <georgeasantanar@gmail.com>
2026-01-24 09:11:53 -0400

    learning the parent-child model in JS, manipulating the DOM

--- jacklyon-dom/Intro/app.js
+++ jacklyon-dom/Intro/app.js
@@ -1,5 +1,9 @@
-let elemento;
-elemento = document.styleSheets
-elemento = document.
-elemento = document.links
-console.log(elemento)
+const elemento = document.getElementsByClassName('list')
+const father = document.querySelector('ul.list-courses > li.list')
+const brothers = document.querySelector('li.list')
+//console.log(father.childElementCount) Counts the amount of childs
+// console.log(father.children) returns the children of a tag (array)
+// console.log(father.children[1].textContent) Method for seeing the childrens
+// console.log(father.firstElementChild) console.log(father.lastElementChild)
+// console.log(father.parentElement.parentElement) 
+// console.log(brothers.nextElementSibling)
