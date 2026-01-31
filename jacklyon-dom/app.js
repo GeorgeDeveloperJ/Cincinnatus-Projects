d15aca855b684e4321e04bd8d3b569f5479cff1e
George Anthony Santana Rosario <georgeasantanar@gmail.com>
2026-01-31 09:28:41 -0400

    Keyboard events, and events listeners

--- jacklyon-dom/app.js
+++ jacklyon-dom/app.js
@@ -1,11 +1,7 @@
-const button = document.getElementById('box')
-function evento() {
-    if (button.onclick) {alert("Enviado!!")}
-    if (button.oncontextmenu) {alert("Info Descriptiva")}
-    if (button.ondblclick) {alert("Doble Click")}
-    if (button.onmousedown) {alert("Button del mouse")}
-    if (button.onmouseenter) {alert("Estas en el button")}
-    if (button.onmouseleave) {alert("Saliste del button")}
-    if (button.onmousemove) {alert("Mouse en movimiento")}
-    if (button.onmouseup) {alert("Dejaste de arrastrar el mouse")}
+const entrada = document.getElementById('input')
+function evento(e) {
+    console.log("Presionaste la tecla " + e.key)
 }
+const evento_anonimo = (e) => {console.log(e)} //Anonymous function for getting events
+
+entrada.addEventListener('keydown', evento)
