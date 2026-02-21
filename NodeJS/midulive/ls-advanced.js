481cd32d2adfc1520805af3a783e90c6443338ef
George Anthony Santana Rosario <georgeasantanar@gmail.com>
2026-02-21 11:34:43 -0400

    finished ls and http projects

--- NodeJS/midulive/ls-advanced.js
+++ NodeJS/midulive/ls-advanced.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
-const picocolors = require('picocolors')
+const pc = require('picocolors')
 
 const folder = process.cwd() ?? '.'
 
@@ -9,7 +9,7 @@ async function ls (folder) {
     try {
         files = await fs.readdir(folder)
     } catch {
-        console.error(`No se pudo leer el directorio ${folder}`)
+        console.error(pc.red(`No se pudo leer el directorio ${folder}`))
         process.exit(1)
     }
 
@@ -25,15 +25,15 @@ async function ls (folder) {
 
         const isDirectory = stats.isDirectory()
         const fileType = isDirectory ? 'd' : '-'
-        const fileSize = stats.size
+        const fileSize = stats.size.toString()
         const fileModified = stats.mtime.toLocaleString()
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
+        return `${pc.white(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
     })
 
     const filesInfo = await Promise.all(filesPromises)
 
-    filesInfo.forEach(fileInfo => console.log(fileInfo))
+            filesInfo.forEach(fileInfo => console.log(fileInfo))
 }
 
 ls(folder)
