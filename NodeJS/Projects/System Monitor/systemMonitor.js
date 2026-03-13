0c7bd2b49c78dc255633ac68dd73783aee186e0c
George Anthony Santana Rosario <georgeasantanar@gmail.com>
2026-03-13 13:01:48 -0400

    feat: add system monitor with Discord webhook alerts
    Implement Node.js script to continuously monitor CPU and RAM usage. Add configurable thresholds via .env and a recursive setTimeout for non-overlapping interval checks. Integrate Axios to send critical alerts to Discord.

---
+++ NodeJS/Projects/System Monitor/systemMonitor.js
@@ -0,0 +1,77 @@
+import os from 'node:os';
+import axios from 'axios';
+import 'dotenv/config';
+
+const ALERT_THRESHOLD = process.env.ALERT_THRESHOLD || 90;
+const CHECK_INTERVAL_MS = process.env.CHECK_INTERVAL_MS  || 1000 * 60 * 5;
+
+function estimatePercRam() {
+  const totalRam = os.totalmem();
+  const freeRam = os.freemem();
+  const usedRam = totalRam - freeRam;
+  const percUsedRam = usedRam / totalRam * 100;
+  return percUsedRam;
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function takeSnapshot(cpus) {
+  return cpus.map(cpu => {
+    const values = Object.values(cpu.times);
+    const total = values.reduce((t, v) => t + v, 0);
+    const idle = cpu.times['idle'];
+    
+    return {total, idle};
+  })
+
+}
+
+async function estimateCPUs() {
+
+  const firstSnapshot = takeSnapshot(os.cpus());
+  await sleep(1000)
+  const secondSnapshot = takeSnapshot(os.cpus());
+
+  const deltaUsage = firstSnapshot.map((item, index) => {
+    const idleDelta = secondSnapshot[index].idle - item.idle;
+    const totalDelta = secondSnapshot[index].total - item.total;
+    const coreUsage = ( 1 - idleDelta / totalDelta ) * 100;
+
+    return { coreUsage };
+  })
+
+  const averageUsage = deltaUsage.reduce((accumulator, core) => core.coreUsage + accumulator, 0) / deltaUsage.length;
+
+  return averageUsage;
+}
+
+async function checkThresholds() {
+  const ram = estimatePercRam();
+  const cpu = await estimateCPUs();
+  const alertRam = ram >= ALERT_THRESHOLD;
+  const alertCPU = cpu >= ALERT_THRESHOLD;
+
+  if (alertRam && alertCPU) {
+    await sendAlert(`**Critical System Alert**\nMemory and CPU Exceeded!\nRAM: ${ram.toFixed(2)}%\nCPU: ${cpu.toFixed(2)}%`);
+  } else if (alertRam) {
+    await sendAlert(`**Memory Pressure**\nRAM usage: ${ram.toFixed(2)}%`);
+  } else if (alertCPU) {
+    await sendAlert(`**CPU Load High**\nCPU usage: ${cpu.toFixed(2)}%`);
+  }
+}
+
+async function sendAlert(message) {
+  try {
+    await axios.post(process.env.DISCORD_WEBHOOK_URL, { content: message });
+    console.log(`[${new Date().toISOString()}] Alert Sent`);
+    } catch (error) {
+    console.error(`[${new Date().toISOString()}] Error sending alert:`, error.message);
+  }
+}
+
+async function startsMonitor() {
+  await checkThresholds();
+  setTimeout(startsMonitor, CHECK_INTERVAL_MS);
+}
+
+startsMonitor();
